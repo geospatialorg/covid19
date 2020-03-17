d03d4f517cdf6a3e4ae6d73718d16f80ac6cfb39
Boboc Cristi <cristi.boboc@soft-union.ro>
2020-03-17 09:18:55 +0200

    map added

---
+++ src/app/dashboard/statistics/coronavirus-europe/coronavirus-europe.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-coronavirus-europe',
+  templateUrl: './coronavirus-europe.component.html',
+  styleUrls: ['./coronavirus-europe.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CoronavirusEuropeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
